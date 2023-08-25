23d0ef52c0b79b73d14fbb8321c4292696b997c6
kolavaliteja <132381999+kolavaliteja@users.noreply.github.com>
2023-08-25 18:54:27 +0530

    Add files via upload

---
+++ CUSTOMER.java
@@ -0,0 +1,135 @@
+
+
+/**
+ * Class CENTRAL_STOCK_DEALER
+ */
+public class CENTRAL_STOCK_DEALER {
+
+  //
+  // Fields
+  //
+
+  private void store_stock_details;
+  
+  //
+  // Constructors
+  //
+  public CENTRAL_STOCK_DEALER () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of store_stock_details
+   * @param newVar the new value of store_stock_details
+   */
+  private void setStore_stock_details (void newVar) {
+    store_stock_details = newVar;
+  }
+
+  /**
+   * Get the value of store_stock_details
+   * @return the value of store_stock_details
+   */
+  private void getStore_stock_details () {
+    return store_stock_details;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void print_bill()
+  {
+  }
+
+
+  /**
+   */
+  public void delivery_product()
+  {
+  }
+
+
+}
+
+
+
+/**
+ * Class CUSTOMER
+ */
+public class CUSTOMER {
+
+  //
+  // Fields
+  //
+
+  private void place_order;
+  private void cancel_order;
+  
+  //
+  // Constructors
+  //
+  public CUSTOMER () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of place_order
+   * @param newVar the new value of place_order
+   */
+  private void setPlace_order (void newVar) {
+    place_order = newVar;
+  }
+
+  /**
+   * Get the value of place_order
+   * @return the value of place_order
+   */
+  private void getPlace_order () {
+    return place_order;
+  }
+
+  /**
+   * Set the value of cancel_order
+   * @param newVar the new value of cancel_order
+   */
+  private void setCancel_order (void newVar) {
+    cancel_order = newVar;
+  }
+
+  /**
+   * Get the value of cancel_order
+   * @return the value of cancel_order
+   */
+  private void getCancel_order () {
+    return cancel_order;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void payment()
+  {
+  }
+
+
+}
